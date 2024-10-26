276352c220336d4f6c7af1322e90b1567b811a1b
Rafael Espinosa <rafaskrullantonio@gmail.com>
2024-10-26 14:54:06 -0600

    First Commit

---
+++ lcds.h
@@ -0,0 +1,18 @@
+// Lambda C Data Structures
+// Library by Rafael Espinosa
+
+/*
+    Stack
+    Queue
+    Linked Lists:
+        Simply Linked List (SLL)
+        Double Linked List (DLL)
+        Simply Circular Linked List (SCLL)
+        Double Circular Linked List (DCLL)
+    Trees:
+        (Coming Soon)
+*/
+
+//////////////////////////////////////////////
+//                   STACK                  //
+//////////////////////////////////////////////
---
+++ test/main.c
@@ -0,0 +1,7 @@
+// Test file for LCDT (Lambda C Data Structures)
+
+#include "../lcds.h"
+
+int main(int argc, char** argv){
+    return 0;
+}
